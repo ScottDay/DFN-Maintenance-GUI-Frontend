417e2818a1d46bf966ab30bc76718c01d04fd95d
Scott Day <17160182@student.curtin.edu.au>
2018-10-14 16:09:03 +1100

    refactor(header): merge index with presenter

--- src/shared/components/Header/index.jsx
+++ src/shared/components/Header/index.jsx
@@ -1,17 +1,76 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import { Manager, Popper, Target } from 'react-popper';
+import styled from 'styled-components';
 
-import Presenter from './presenter';
+import Button from '@material-ui/core/Button';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import * as service from './service';
 import store from './store';
 
 
+const StyledButton = styled(Button)`
+    height: inherit !important;
+    padding-top: 18px !important;
+    padding-bottom: 18px !important;
+`;
+
+const StyledMoreVertIcon = styled(MoreVertIcon)`
+	color: #a1a1a1;
+`;
+
+const StyledMenuList = styled(MenuList)`
+    height: inherit;
+`;
+
+
+@observer
 export default class Header extends React.Component {
 	render() {
 		return (
-			<Presenter
-				service={service}
-				store={store}
-			/>
+			<section className='app-header'>
+				<div className='app-header-inner bg-color-dark'>
+					<div className='top-nav-right'>
+						<ClickAwayListener onClickAway={() => (store.isOpen ? store.setIsOpen(false) : null)}>
+							<Manager>
+								<Target>
+									<StyledButton
+										aria-owns={store.isOpen ? 'menu-list-grow' : null}
+										aria-haspopup='true'
+										onClick={() => (store.isOpen ? store.setIsOpen(false) : store.setIsOpen(true))}
+									>
+										<StyledMoreVertIcon />
+									</StyledButton>
+								</Target>
+								<Popper
+									placement='bottom-start'
+									eventsEnabled={store.isOpen}
+								>
+									<Grow
+										in={store.isOpen}
+										id='menu-list-grow'
+										style={{transformOrigin: '0 0 0'}}
+									>
+										<Paper>
+											<StyledMenuList role='menu'>
+												<MenuItem onClick={() => service.logout()}>
+													Logout
+												</MenuItem>
+											</StyledMenuList>
+										</Paper>
+									</Grow>
+								</Popper>
+							</Manager>
+						</ClickAwayListener>
+					</div>
+				</div>
+			</section>
 		);
 	}
 }
--- src/shared/components/Header/service.js
+++ src/shared/components/Header/service.js
@@ -3,13 +3,8 @@ import { sessionAction } from 'actions';
 import store from './store';
 
 
-function logout() {
+// eslint-disable-next-line import/prefer-default-export
+export function logout() {
 	store.setIsOpen(false);
 	sessionAction.logout();
-}
-
-
-export {
-	// eslint-disable-next-line import/prefer-default-export
-	logout
 }
